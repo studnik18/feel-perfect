8ac546f52af9e92bcdd030b29fa5e8539e15e10e
Maciej <maciejstudniarz@gmail.com>
2018-07-28 17:59:23 +0200

    Finished without google maps

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,59 @@
-
 $( function() {
 	$( "#main" ).tabs();
+	var settings = {
+		mobileFirst: true,
+		dots: true,
+		arrows: false,
+		slidesToShow: 1,
+		infinite: true,
+	  responsive: [
+	    {
+	      breakpoint: 570,
+	      settings: {
+	      	arrows: true
+	      }
+	    },
+	    {
+	    	breakpoint: 768,
+	    	settings: 'unslick'
+	    }
+
+	  ]
+	}
+	$('.about-me-slider').slick(settings);
+  $(window).on('resize', function() {
+    if ($(window).width() > 767) {
+      if ($('.about-me-slider').hasClass('slick-initialized')) {
+        $('.about-me-slider').slick('unslick');
+      }
+      return
+    }
+    if (!$('.about-me-slider').hasClass('slick-initialized')) {
+      return $('.about-me-slider').slick(settings);
+    }
+  });
 });
 
 var fb = document.querySelector('.fb-like');
-if (window.innerWidth > 800) {
+if (window.innerWidth > 768) {
 	fb.setAttribute('data-size', 'large');
 	fb.setAttribute('data-show-faces', 'true');
 }
 if (window.innerWidth < 500) {
 	fb.setAttribute('data-width', (window.innerWidth * 0.9))
 }
+
+var tabs = document.querySelectorAll('.menu-item a');
+
+function toggleActive(event) {
+	tabs.forEach(function(tab){
+		tab.setAttribute('data-active', 'false');
+	})
+	if (event.target.getAttribute('data-active') !== 'active') {
+		event.target.setAttribute('data-active', 'true');
+	}
+}
+
+tabs.forEach(function(tab){
+	tab.addEventListener('click', toggleActive)
+});
